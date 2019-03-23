52e039b1517c3e606cf3c14df8046a09a8512869
Remtasya <hamilton_92_@hotmail.com>
2019-03-23 13:33:31 +1100

    Revert "changed the code to include the noisy net modifications - note however that training convergence is odd as can be seen in the training file"
    This reverts commit 5710052536c88d8fd7f0f87ee8fcad3b03715a5c.

--- model.py
+++ model.py
@@ -1,9 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from custom_noisy_net_layer import custom_linear_noisy_net
-from custom_noisy_net_layer_external import NoisyFactorizedLinear
-
 
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
@@ -19,13 +16,13 @@ class QNetwork(nn.Module):
         super(QNetwork, self).__init__()
         # input
         self.seed = torch.manual_seed(seed)
-        self.fc1 = custom_linear_noisy_net(state_size, 64)
+        self.fc1 = nn.Linear(state_size, 64)
         # advantage
-        self.ad1 = custom_linear_noisy_net(64, 32)
-        self.ad2 = custom_linear_noisy_net(32, action_size)
+        self.ad1 = nn.Linear(64, 32)
+        self.ad2 = nn.Linear(32, action_size)
         # value
-        self.va1 = custom_linear_noisy_net(64, 32)
-        self.va2 = custom_linear_noisy_net(32, 1)
+        self.va1 = nn.Linear(64, 32)
+        self.va2 = nn.Linear(32, 1)
 
     def forward(self, state):
         """Build a network that maps state -> action values."""
